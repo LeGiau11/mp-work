18dd2e786b6470884173d04643f513e8793238b3
LeGiau11 <levangiaucntt@gmail.com>
2024-09-16 00:11:20 +0700

    create skeleton

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import Input from "./Input";
+import Header from "./Header";
+import Footer from "./Footer";
+import Sidebar from "./Sidebar";
+
+export { Input, Header, Footer, Sidebar };
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,3 @@
+export default function Login() {
+  return <div>Day la login</div>;
+}
