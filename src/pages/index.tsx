bb32f742ffe2b5a19790b711855f2dc655977a97
LeGiau11 <levangiaucntt@gmail.com>
2024-09-30 22:14:12 +0700

    add loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,16 @@ import Login from "./login";
 
 export default function Layout() {
   const [token, setToken] = useState<string | null>("");
+  const [loading, setLoading] = useState<boolean>(true);
+
   const router = useRouter();
   useEffect(() => {
     createInitUser();
     const token = localStorage.getItem("token");
-    setToken(token);
+
+    if (token) setToken(token);
+
+    setLoading(false);
 
     if (!token) router.push("/login");
   }, []);
@@ -38,20 +43,47 @@ export default function Layout() {
     router.push("/login");
   };
 
+  if (loading) return <div>Loading....</div>;
+
   return (
     <>
       <Head>
         <title>Home Page - My Next.js App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Welcome to my awesome Next.js app!" />
       </Head>
       {!token ? <Login /> : null}
       {token ? (
-        <div className={`${styles.page}`}>ß
-          <Button  variant="danger" type="button" onClick={handleClick}>
+        <div className={`${styles.page}`}>
+          ß
+          <Button
+            icon={
+              <svg
+                className="w-6 h-6 text-gray-800 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m5 15 7-7 7 7"
+                />
+              </svg>
+            }
+            variant="danger"
+            type="button"
+            onClick={handleClick}
+          >
             Logout
           </Button>
-          <br/>
-          <Input/>
+          <br />
+          <Input />
           {/* <Header /> 
         <main className={styles.main}>
           <Sidebar />
