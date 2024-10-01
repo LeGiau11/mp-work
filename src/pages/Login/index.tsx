77df82fc505c60b023f752812e9f16ded605fadd
LeGiau11 <levangiaucntt@gmail.com>
2024-10-02 00:09:40 +0700

    fix page login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,12 @@
 import { useRouter } from "next/router";
 import { useFormik } from "formik";
+import Image from "next/image";
 
 import { Button, Input, InputPassword } from "@/components";
 import { RequestLogin } from "./interface";
 import styles from "./Login.module.scss";
 import { ResponseData } from "@/common";
+import { Apple, Facebook, Google, Language } from "@/svg";
 
 export default function Login() {
   const router = useRouter();
@@ -36,53 +38,90 @@ export default function Login() {
 
   return (
     <section className={styles.container}>
-      <form onSubmit={formik.handleSubmit}>
-        <div className={styles.wrapper}>
-          <div className={styles.loginTitle}>
-            <h1>Login</h1>
-            <p>Hey, Enter your detail to get sign in to your account</p>
-          </div>
-          <div className={styles.loginInput}>
-            <Input
-              placeholder="Enter Username"
-              className={styles.username}
-              type="text"
-              name="username"
-              value={formik.values.username}
-              onChange={formik.handleChange}
-            />
-            <InputPassword
-              className={styles.password}
-              placeholder="Enter Password"
-              name="password"
-              value={formik.values.password}
-              onChange={formik.handleChange}
-            />
-            <Button variant="text" type="button">
-              Having trouble in sign in?
-            </Button>
-          </div>
-          <Button
-            disabled={formik.isSubmitting}
-            variant="contained"
-            type="submit"
-          >
-            Sign in
-          </Button>
-          <span>Or sign in width</span>
-          <div className="referenceSocial">
-            <Button type="button">Google</Button>
-            <Button type="button">Apple ID</Button>
-            <Button type="button">Facebook</Button>
-          </div>
-          <div>
-            Don&apos;t have an account?
-            <Button variant="text" type="button">
-              <strong>Request Now</strong>
-            </Button>
-          </div>
+      <div className={styles.logo}>
+        <div className="left">
+          <Image
+            width={220}
+            height={48}
+            src="/images/logo.png"
+            alt="logo.png"
+          />
         </div>
-      </form>
+        <div className={styles.logoRight}>
+          <Button variant="text" type="button" icon={<Language />} />
+          <Button variant="text" type="button">
+            Sign up
+          </Button>
+          <Button variant="contained" type="button">
+            Request Demo
+          </Button>
+        </div>
+      </div>
+      <div className={styles.image}>
+        <Image
+          width={1200}
+          height={500}
+          src="/images/login.png"
+          alt="login.png"
+        />
+      </div>
+      <div className={styles.content}>
+        <form onSubmit={formik.handleSubmit}>
+          <div className={styles.wrapper}>
+            <div className={styles.loginTitle}>
+              <h1>Welcome back!</h1>
+              <p>Hey, Enter your detail to get sign in to your account</p>
+            </div>
+            <div className={styles.loginInput}>
+              <Input
+                placeholder="Enter Username"
+                className={styles.username}
+                type="text"
+                name="username"
+                value={formik.values.username}
+                onChange={formik.handleChange}
+              />
+              <InputPassword
+                className={styles.password}
+                placeholder="Enter Password"
+                name="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+              />
+              <Button variant="text" type="button">
+                Having trouble in sign in?
+              </Button>
+            </div>
+            <Button
+              disabled={formik.isSubmitting}
+              variant="contained"
+              type="submit"
+            >
+              Sign in
+            </Button>
+            <span>Or sign in width</span>
+            <div className={styles.social}>
+              <Button variant="outlined" icon={<Google />} type="button">
+                Google
+              </Button>
+
+              <Button variant="outlined" icon={<Apple />} type="button">
+                Apple
+              </Button>
+
+              <Button variant="outlined" icon={<Facebook />} type="button">
+                Facebook
+              </Button>
+            </div>
+            <div>
+              Don&apos;t have an account?
+              <Button variant="text" type="button">
+                <strong>Request Now</strong>
+              </Button>
+            </div>
+          </div>
+        </form>
+      </div>
     </section>
   );
 }
