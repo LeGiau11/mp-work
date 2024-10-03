83f623e3daeb2a8e3cb36602ce61beef4288436a
LeGiau11 <levangiaucntt@gmail.com>
2024-10-03 23:41:11 +0700

    test code

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -41,8 +41,8 @@ export default function Login() {
       <div className={styles.logo}>
         <div className="left">
           <Image
-            width={140}
-            height={40}
+            width={215}
+            height={45}
             src="/images/logo.png"
             alt="logo.png"
           />
@@ -57,82 +57,84 @@ export default function Login() {
           </Button>
         </div>
       </div>
-      <div className={styles.image}>
-        <Image
-          width={1255}
-          height={500}
-          src="/images/login.png"
-          alt="login.png"
-        />
-      </div>
-      <div className={styles.content}>
-        <form onSubmit={formik.handleSubmit}>
-          <div className={styles.loginTitle}>
-            <h1>Welcome back!</h1>
-            <p>Hey, Enter your detail to get sign in to your account</p>
-          </div>
-          <div className={styles.loginInput}>
-            <div>
-              <Input
-                placeholder="Enter Username"
-                className={styles.username}
-                type="text"
-                name="username"
-                value={formik.values.username}
-                onChange={formik.handleChange}
-              />
-              <span className={styles.errorMessage}>
-                Tên người dùng không được để trống.
-              </span>
+      <div className={styles.wrapper}>
+        <div className={styles.image}>
+          <Image
+            width={1140}
+            height={468}
+            src="/images/login_page.png"
+            alt="login.png"
+          />
+        </div>
+        <div className={styles.content}>
+          <form onSubmit={formik.handleSubmit}>
+            <div className={styles.loginTitle}>
+              <h1>Welcome back!</h1>
+              <p>Hey, Enter your detail to get sign in to your account</p>
             </div>
-            <div>
-              <InputPassword
-                className={styles.password}
-                placeholder="Enter Password"
-                name="password"
-                value={formik.values.password}
-                onChange={formik.handleChange}
-              />
-              <span className={styles.errorMessage}>
-                Tên người dùng không được để trống.
-              </span>
+            <div className={styles.loginInput}>
+              <div>
+                <Input
+                  placeholder="Enter Username"
+                  className={styles.username}
+                  type="text"
+                  name="username"
+                  value={formik.values.username}
+                  onChange={formik.handleChange}
+                />
+                <span className={styles.errorMessage}>
+                  Tên người dùng không được để trống.
+                </span>
+              </div>
+              <div>
+                <InputPassword
+                  className={styles.password}
+                  placeholder="Enter Password"
+                  name="password"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                />
+                <span className={styles.errorMessage}>
+                  Tên người dùng không được để trống.
+                </span>
+              </div>
+              <Button
+                className={styles.forgotPassword}
+                variant="text"
+                type="button"
+              >
+                Forgot password?
+              </Button>
             </div>
             <Button
-              className={styles.forgotPassword}
-              variant="text"
-              type="button"
+              disabled={formik.isSubmitting}
+              variant="contained"
+              type="submit"
             >
-              Forgot password?
+              Sign in
             </Button>
-          </div>
-          <Button
-            disabled={formik.isSubmitting}
-            variant="contained"
-            type="submit"
-          >
-            Sign in
-          </Button>
-          <span className={styles.signInWith}>Or sign in with</span>
-          <div className={styles.social}>
-            <Button variant="outlined" icon={<Google />} type="button">
-              Google
-            </Button>
+            <span className={styles.signInWith}>Or sign in with</span>
+            <div className={styles.social}>
+              <Button variant="outlined" icon={<Google />} type="button">
+                Google
+              </Button>
 
-            <Button variant="outlined" icon={<Apple />} type="button">
-              Apple
-            </Button>
+              <Button variant="outlined" icon={<Apple />} type="button">
+                Apple
+              </Button>
 
-            <Button variant="outlined" icon={<Facebook />} type="button">
-              Facebook
-            </Button>
-          </div>
-          <div className={styles.haveAnAccount}>
-            Don&apos;t have an account?
-            <Button variant="text" type="button">
-              Request Now
-            </Button>
-          </div>
-        </form>
+              <Button variant="outlined" icon={<Facebook />} type="button">
+                Facebook
+              </Button>
+            </div>
+            <div className={styles.haveAnAccount}>
+              Don&apos;t have an account?
+              <Button variant="text" type="button">
+                Request Now
+              </Button>
+            </div>
+          </form>
+        </div>
       </div>
     </section>
   );
