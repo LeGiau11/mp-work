a25cf38a1254cfcaecd3f89f02a661d31d75a5b6
LeGiau11 <levangiaucntt@gmail.com>
2024-10-05 22:18:23 +0700

    fix css login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -36,115 +36,6 @@ export default function Login() {
     }
   };
 
-  // return (
-  //   <section className={styles.container}>
-  //     <div className={styles.logo}>
-  //       <div className="left">
-  //         <Image
-  //           width={215}
-  //           height={45}
-  //           src="/images/logo.png"
-  //           alt="logo.png"
-  //         />
-  //       </div>
-  //       <div className={styles.logoRight}>
-  //         <Button variant="text" type="button" icon={<Language />} />
-  //         <Button variant="text" type="button">
-  //           Sign up
-  //         </Button>
-  //         <Button variant="contained" type="button">
-  //           Request Demo
-  //         </Button>
-  //       </div>
-  //     </div>
-  //     <div className={styles.wrapper}>
-  //       <div className={styles.image}>
-  //         <Image
-  //           width={1140}
-  //           height={468}
-  //           src="/images/login_page.png"
-  //           alt="login.png"
-  //         />
-  //       </div>
-  //       <div className={styles.content}>
-  //         <form onSubmit={formik.handleSubmit}>
-  //           <div className={styles.loginTitle}>
-  //             <h1>Welcome back!</h1>
-  //             <p>Please enter your details </p>
-  //           </div>
-  //           <div className={styles.loginInput}>
-  //             <div>
-  //               <Input
-  //                 placeholder="Enter Username"
-  //                 className={styles.username}
-  //                 type="text"
-  //                 name="username"
-  //                 value={formik.values.username}
-  //                 onChange={formik.handleChange}
-  //               />
-  //               <span className={styles.errorMessage}>
-  //                 Tên người dùng không được để trống.
-  //               </span>
-  //             </div>
-  //             <div>
-  //               <InputPassword
-  //                 className={styles.password}
-  //                 placeholder="Enter Password"
-  //                 name="password"
-  //                 value={formik.values.password}
-  //                 onChange={formik.handleChange}
-  //               />
-  //               <span className={styles.errorMessage}>
-  //                 Tên người dùng không được để trống.
-  //               </span>
-  //             </div>
-  //             <Button
-  //               className={styles.forgotPassword}
-  //               variant="text"
-  //               type="button"
-  //             >
-  //               Forgot password?
-  //             </Button>
-  //           </div>
-  //           <Button
-  //             disabled={formik.isSubmitting}
-  //             variant="contained"
-  //             type="submit"
-  //           >
-  //             Sign in
-  //           </Button>
-  //           <span className={styles.signInWith}>Or sign in with</span>
-  //           <div className={styles.social}>
-  //             <Button
-  //               variant="outlined"
-  //               icon={<Google />}
-  //               type="button"
-  //             ></Button>
-
-  //             <Button
-  //               variant="outlined"
-  //               icon={<Apple />}
-  //               type="button"
-  //             ></Button>
-
-  //             <Button
-  //               variant="outlined"
-  //               icon={<Facebook />}
-  //               type="button"
-  //             ></Button>
-  //           </div>
-  //           <div className={styles.haveAnAccount}>
-  //             Don&apos;t have an account?
-  //             <Button variant="text" type="button">
-  //               Request Now
-  //             </Button>
-  //           </div>
-  //         </form>
-  //       </div>
-  //     </div>
-  //   </section>
-  // );
-
   return (
     <div className={styles.container}>
       <div className={styles.left}>
@@ -157,12 +48,12 @@ export default function Login() {
           />
         </div>
         <div className={styles.loginForm}>
-          <div className={styles.title}>
-            <h1>Welcome back!</h1>
-            <h3>Please enter your details </h3>
-          </div>
           <form onSubmit={formik.handleSubmit}>
             <div className={styles.wrapper}>
+              <div className={styles.title}>
+                <h1>Welcome back!</h1>
+                <h3>Please enter your details </h3>
+              </div>
               <div className={styles.inputs}>
                 <div className={styles.wrapperInput}>
                   <label>Email Address</label>
